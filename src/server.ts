bb67459a6861484da7aaf8ac202ee4eea30fcf3d
Gustavo Almeida <gustavo@galileosoft.com.br>
2021-06-03 15:00:07 -0300

    Estrutura base: Server.ts (era pra ter ido nos commits iniciais)

---
+++ src/server.ts
@@ -0,0 +1,34 @@
+import 'reflect-metadata';
+import express, { Request, Response, NextFunction } from 'express';
+import cors from 'cors';
+import 'express-async-errors';
+import uploadConfig from './config/upload';
+import routes from './routes';
+import AppError from './errors/AppError';
+import './database';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/files', express.static(uploadConfig.directory));
+app.use(routes);
+
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
+
+  console.error(err);
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal Server Error',
+  });
+});
+app.listen(1919, () => {
+  console.log('Server Started on port 1919');
+});
