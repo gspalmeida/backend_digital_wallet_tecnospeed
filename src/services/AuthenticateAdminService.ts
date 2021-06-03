bb526b3ea177e8a8f7738850accad7a2cd35eb8e
Gustavo Almeida <gustavo@galileosoft.com.br>
2021-06-02 21:28:09 -0300

    Service de autenticação de admin

---
+++ src/services/AuthenticateAdminService.ts
@@ -0,0 +1,45 @@
+/* eslint-disable camelcase */
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
+import Admin from '../models/Admin';
+import authConfig from '../config/auth';
+import AppError from '../errors/AppError';
+
+interface Request {
+  email: string;
+  password: string;
+}
+interface Response {
+  admin: Admin;
+  token: string;
+}
+
+class AuthenticateAdminService {
+  public async execute({ email, password }: Request): Promise<Response> {
+    const adminRepository = getRepository(Admin);
+
+    const admin = await adminRepository.findOne({ where: { email } });
+    if (admin) {
+      console.log(admin);
+      const passwordMatched = await compare(password, admin.password);
+      if (!passwordMatched) {
+        throw new AppError('Incorrect email/password combination', 401);
+      }
+      const { secret, expiresIn } = authConfig.jwt;
+
+      const token = sign({}, secret, {
+        subject: admin.id,
+        expiresIn,
+      });
+
+      return {
+        admin,
+        token,
+      };
+    }
+    return { admin: {} as Admin, token: '' };
+  }
+}
+
+export default AuthenticateAdminService;
