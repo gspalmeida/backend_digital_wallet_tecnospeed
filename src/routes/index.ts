65e5eb8f9a0cb6829efb6d2f4e2e9eaf859baef6
Gustavo Almeida <gustavo@galileosoft.com.br>
2021-06-03 15:15:18 -0300

    Rota: Adição das novas rotas ao index

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,14 @@ import authRouter from './auth.routes';
 import usersRouter from './users.routes';
 import adminsRouter from './admins.routes';
 import isAdmin from '../middlewares/isAdmin';
+import movementsRouter from './movements.routes';
+import movementCategoriesRouter from './movementCategories.routes';
 
 const routes = Router();
 routes.use('/auth', authRouter);
-routes.use('/admins', isAdmin, adminsRouter);
 routes.use('/users', usersRouter);
+routes.use('/admins', isAdmin, adminsRouter);
+routes.use('/movements', movementsRouter);
+routes.use('/categories', movementCategoriesRouter);
 
 export default routes;
