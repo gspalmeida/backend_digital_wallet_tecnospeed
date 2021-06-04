fa6e6d174640a3ec2165cd2519f9d90b2cfff94c
Gustavo Almeida <gustavo@galileosoft.com.br>
2021-06-03 23:15:11 -0300

    Rota: Criação de Categorias

--- src/routes/movementCategories.routes.ts
+++ src/routes/movementCategories.routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { getRepository } from 'typeorm';
 import ensureAuthenticated from '../middlewares/ensureAuthenticated';
 import FinancialMovementCategory from '../models/FinancialMovementCategory';
+import CreateFinancialMovementCategoryService from '../services/CreateFinancialMovementCategoryService';
 
 const movementCategoriesRouter = Router();
 
@@ -16,6 +17,23 @@ movementCategoriesRouter.get(
 
     const movementCategory = await financialMovementCategoryRepository.find();
     return response.json(movementCategory);
+  },
+);
+
+movementCategoriesRouter.post(
+  '/',
+  ensureAuthenticated,
+  async (request, response) => {
+    const { categoryName } = request.body;
+    const createFinancialMovementCategory = new CreateFinancialMovementCategoryService();
+
+    const financialMovementCategory = await createFinancialMovementCategory.execute(
+      {
+        categoryName,
+      },
+    );
+
+    return response.json(financialMovementCategory);
   },
 );
 
