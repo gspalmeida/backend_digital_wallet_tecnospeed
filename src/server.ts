b602f9f2ad5bfa712000c7aa21730053a79aa093
Gustavo Almeida <gustavo@galileosoft.com.br>
2021-06-07 00:13:09 -0300

    Listen to process.env.PORT (heroku requirement)

--- src/server.ts
+++ src/server.ts
@@ -29,6 +29,6 @@ app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
     message: 'Internal Server Error',
   });
 });
-app.listen(1919, () => {
-  console.log('Server Started on port 1919');
+app.listen(process.env.PORT || 1919, () => {
+  console.log('Server Started on port', process.env.POR);
 });
