d4fa9104530c5e6bee9f0811925bd99b33d1655f
Gustavo Almeida <gustavo@galileosoft.com.br>
2021-06-07 00:02:28 -0300

    Configurações Deploy Heroku

--- ormconfig.js
+++ ormconfig.js
@@ -1,12 +1,14 @@
+console.log('process.env.TYPEORM_DATABASE', process.env.TYPEORM_DATABASE);
 module.exports = {
   type: 'postgres',
-  host: process.env.TYPEORM_HOST,
-  port: process.env.TYPEORM_PORT,
-  username: process.env.TYPEORM_USERNAME,
-  password: process.env.TYPEORM_PASSWORD,
-  database: process.env.TYPEORM_DATABASE,
-  entities: [process.env.TYPEORM_ENTITIES],
-  migrations: [process.env.TYPEORM_MIGRATIONS],
+  url: process.env.TYPEORM_DATABASE,
+  ssl: true,
+  entities: [
+    process.env.TYPEORM_ENTITIES
+  ],
+  migrations: [
+    process.env.TYPEORM_MIGRATIONS
+  ],
   cli: {
     migrationsDir: process.env.TYPEORM_MIGRATIONS_DIR,
   },
